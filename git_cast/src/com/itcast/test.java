0c0cd88abc9f869955cd84e979118bf85362cbb1
feng <2277212152@qq.com>
2018-09-25 20:19:15 +0800

    first commit

---
+++ git_cast/src/com/itcast/test.java
@@ -0,0 +1,10 @@
+package com.itcast;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("vdfgsdg");
+        System.out.println("vdfgsdg");
+        System.out.println("vdfgsdg");
+        System.out.println("vdfgsdg");
+    }
+}
